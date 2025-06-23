4ec20365e424fba6b9ce48d3eec3df88870a7f2a
mary kenny <marykenny2704@yahoo.com>
2025-06-23 15:11:23 +0200

    initial commit

---
+++ src/main/java/com/femcoders/toy_store_backend/ToyStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.femcoders.toy_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyStoreBackendApplication.class, args);
+	}
+
+}
