4ea0c3f8109feec43b1128da84552c87cca8b02b
mary kenny <marykenny2704@yahoo.com>
2025-06-26 15:38:09 +0200

    feat: create user entity

--- src/main/java/com/femcoders/toy_store_backend/models/User.java
+++ src/main/java/com/femcoders/toy_store_backend/models/User.java
@@ -1,4 +1,18 @@
 package com.femcoders.toy_store_backend.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String username;
+    private String email;
+    private String password;
+
+
 }
