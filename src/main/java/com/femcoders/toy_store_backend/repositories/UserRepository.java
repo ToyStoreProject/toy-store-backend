a34751df61c3b364a355d537a5297ba502d3ca45
Mariya Byehan <maria_byehan@yahoo.com>
2025-06-30 14:15:44 +0200

    feat: update UserRepository

--- src/main/java/com/femcoders/toy_store_backend/repositories/UserRepository.java
+++ src/main/java/com/femcoders/toy_store_backend/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package com.femcoders.toy_store_backend.repositories;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.femcoders.toy_store_backend.models.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
