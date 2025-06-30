6f0cebd3e5f696666029f9b65c06ecfb6c2a877d
AnaCarinaPP <ancari1989@gmail.com>
2025-06-30 14:28:20 +0200

    feat: update product repository

--- src/main/java/com/femcoders/toy_store_backend/repositories/ProductRepository.java
+++ src/main/java/com/femcoders/toy_store_backend/repositories/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.femcoders.toy_store_backend.repositories;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.femcoders.toy_store_backend.models.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
