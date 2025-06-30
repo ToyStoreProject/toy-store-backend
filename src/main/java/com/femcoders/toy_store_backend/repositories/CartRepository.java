db0ff8bb7da1e5b3f299684692f8d7378275041b
AnaCarinaPP <ancari1989@gmail.com>
2025-06-30 14:33:19 +0200

    feat: update cart and cartiItem repositories

--- src/main/java/com/femcoders/toy_store_backend/repositories/CartRepository.java
+++ src/main/java/com/femcoders/toy_store_backend/repositories/CartRepository.java
@@ -1,4 +1,9 @@
 package com.femcoders.toy_store_backend.repositories;
 
-public interface CartRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.femcoders.toy_store_backend.models.Cart;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
