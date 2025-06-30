85509628afd14dd81dc808376c286489cbc446b2
AnaCarinaPP <ancari1989@gmail.com>
2025-06-30 12:40:31 +0200

    feat: create cart entity

--- src/main/java/com/femcoders/toy_store_backend/models/Cart.java
+++ src/main/java/com/femcoders/toy_store_backend/models/Cart.java
@@ -1,4 +1,50 @@
 package com.femcoders.toy_store_backend.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Entity
+@Table(name= "carts")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    private Product product;
+
+    @Positive
+    @NotNull
+    @Transient
+    private double totalPrice;
+
+    public Cart() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
