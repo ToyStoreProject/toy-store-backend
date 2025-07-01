88f47fa4539a8315d52bc0f251a92bbe184b2388
mary kenny <marykenny2704@yahoo.com>
2025-07-01 14:20:03 +0200

    feat:add getAllUsers method to service and controller

--- src/main/java/com/femcoders/toy_store_backend/services/UserService.java
+++ src/main/java/com/femcoders/toy_store_backend/services/UserService.java
@@ -1,4 +1,23 @@
 package com.femcoders.toy_store_backend.services;
 
+
+import com.femcoders.toy_store_backend.models.User;
+import com.femcoders.toy_store_backend.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    public final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
 }
