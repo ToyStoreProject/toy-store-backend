9ae6602496fd9ae9a3c7ef8489fd09a1b0ef9b1f
Bruna Sonda <bsonda@gmail.com>
2025-07-03 09:43:30 +0200

    feat: add getAllProducts method to Product Service and Controller

--- src/main/java/com/femcoders/toy_store_backend/controllers/ProductController.java
+++ src/main/java/com/femcoders/toy_store_backend/controllers/ProductController.java
@@ -1,4 +1,28 @@
 package com.femcoders.toy_store_backend.controllers;
 
+import com.femcoders.toy_store_backend.dtos.product.ProductResponse;
+import com.femcoders.toy_store_backend.models.Product;
+import com.femcoders.toy_store_backend.services.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
+    }
 }
--- src/main/java/com/femcoders/toy_store_backend/services/ProductService.java
+++ src/main/java/com/femcoders/toy_store_backend/services/ProductService.java
@@ -1,4 +1,21 @@
 package com.femcoders.toy_store_backend.services;
 
+import com.femcoders.toy_store_backend.dtos.product.ProductResponse;
+import com.femcoders.toy_store_backend.models.Product;
+import com.femcoders.toy_store_backend.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
 }
