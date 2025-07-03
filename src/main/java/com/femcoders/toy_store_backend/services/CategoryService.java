a7e537d0bcf441c3c6c1bf5a2bf5c7a4b93f246f
Mariya Byehan <maria_byehan@yahoo.com>
2025-07-03 11:59:41 +0200

    feat: add all methods to Service and Controller

--- src/main/java/com/femcoders/toy_store_backend/services/CategoryService.java
+++ src/main/java/com/femcoders/toy_store_backend/services/CategoryService.java
@@ -1,4 +1,37 @@
 package com.femcoders.toy_store_backend.services;
 
+import com.femcoders.toy_store_backend.models.Category;
+import com.femcoders.toy_store_backend.models.Product;
+import com.femcoders.toy_store_backend.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    public Category addCategory(Category newCategory){
+        return categoryRepository.save(newCategory);
+    }
+
+    public Optional<Category> getCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Category updateCategory(Category category, Category updatedCategory){
+        category.setName(updatedCategory.getName());
+        return categoryRepository.save(category);
+    }
+
+    public void deleteCategory(Category category) {
+        categoryRepository.delete(category);
+    }
 }
