285ffc5fa5c85a01b2d7f9aacbc824c134b65432
AnaCarinaPP <ancari1989@gmail.com>
2025-07-03 11:15:48 +0200

    feat: add getProductById, updateProduct, deleteProduct methods to service and controller

--- src/main/java/com/femcoders/toy_store_backend/services/ProductService.java
+++ src/main/java/com/femcoders/toy_store_backend/services/ProductService.java
@@ -2,10 +2,12 @@ package com.femcoders.toy_store_backend.services;
 
 import com.femcoders.toy_store_backend.dtos.product.ProductResponse;
 import com.femcoders.toy_store_backend.models.Product;
+import com.femcoders.toy_store_backend.models.User;
 import com.femcoders.toy_store_backend.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -18,4 +20,25 @@ public class ProductService {
     public List<Product> getAllProducts(){
         return productRepository.findAll();
     }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product addProduct(Product newProduct){
+        return productRepository.save(newProduct);
+    }
+
+    public Product updateProduct(Product product, Product updatedProduct){
+        product.setName(updatedProduct.getName());
+        product.setImageUrl(updatedProduct.getImageUrl());
+        product.setFeatured(updatedProduct.isFeatured());
+        product.setPrice(updatedProduct.getPrice());
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Product product) {
+        productRepository.delete(product);
+    }
+
 }
