7d6218b6398e00041577ceb73b04bbcc34db9083
Mariya Byehan <maria_byehan@yahoo.com>
2025-07-04 14:46:20 +0200

    feat: add methods add and delete to Service y Controller

--- src/main/java/com/femcoders/toy_store_backend/controllers/CartController.java
+++ src/main/java/com/femcoders/toy_store_backend/controllers/CartController.java
@@ -1,4 +1,30 @@
 package com.femcoders.toy_store_backend.controllers;
 
+import com.femcoders.toy_store_backend.services.CartService;
+import com.femcoders.toy_store_backend.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cart")
 public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @PostMapping("/{userId}/add/{productId}")
+    public ResponseEntity<?> addProductToCart(@PathVariable Long userId, @PathVariable Long productId) {
+        cartService.addProduct(userId, productId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{userId}/delete/{productId}")
+    public ResponseEntity<?> deleteProductFromCart(@PathVariable Long userId, @PathVariable Long productId) {
+        cartService.deleteProduct(userId, productId);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/femcoders/toy_store_backend/services/CartService.java
+++ src/main/java/com/femcoders/toy_store_backend/services/CartService.java
@@ -1,4 +1,13 @@
 package com.femcoders.toy_store_backend.services;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CartService {
+
+    public void addProduct(Long userId, Long productId) {
+    }
+
+    public void deleteProduct(Long userId, Long productId) {
+
+    }
 }
